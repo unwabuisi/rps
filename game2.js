49a4a3a417e6feffe9ecc87f1cc1cd70bdca63c3
unwabuisi <unwabuisi@utexas.edu>
2018-08-22 19:53:44 -0500

    style changes and momentjs added to chat box

--- game2.js
+++ game2.js
@@ -16,6 +16,8 @@ var config = {
 
   var database = firebase.database();
   var gameRef = database.ref('/game/');
+  var chatRef = database.ref('/game/chat');
+  var turn = 0;
 
 
 //===================================== FUNCTIONS
@@ -25,7 +27,6 @@ var nameSubmitted = function () {
     var username = $("#username").val().trim();
 
     gameRef.once('value', function (snapshot) {
-
         //if player1 does not exist - add player 1
         if (!snapshot.child('player1').exists()) {
             gameRef.child('player1').update({
@@ -35,7 +36,6 @@ var nameSubmitted = function () {
                 choice:""
             });
         }
-
         //if player 2 does not exist - add player 2
         else if (!snapshot.child('player2').exists()) {
             gameRef.child('player2').update({
@@ -46,10 +46,10 @@ var nameSubmitted = function () {
             });
             gameRef.child('chat').push({
                 name:"",
-                message:"Welcome to Rock, Paper, Scissors"
+                message:"Welcome to Rock Paper Scissors"
             });
             gameRef.update({turn:0});
-
+            turn = parseInt(snapshot.val().turn);
         }
 
     });
@@ -65,113 +65,213 @@ $('#username').keypress(function(e){
         }
 });
 
+//fills in boxes for usernames wins and losses
+function fillBox (data,boxToFill) {
+
+    var rock = $("<button>").text("Rock").attr({
+    'data-attribute':'rock',
+    'class':'btn btn-light'
+    });
+
+    var paper = $("<button>").text("Paper").attr({
+    'data-attribute':'paper',
+    'class':'btn btn-light'
+    });
+
+    var scissors = $("<button>").text('Scissors').attr({
+    'data-attribute':'scissors',
+    'class':'btn btn-light'
+    });
+
+    playerName = data.val().name;
+
+    $("#" + boxToFill + "Name").html("<h2>"+playerName+"</h2>");
+
+    $("#" + boxToFill + "Selections").append(rock);
+    rock.after(paper);
+    paper.after(scissors);
+
+    $("#" + boxToFill + "Score").html("Wins: " + data.val().wins+" | Losses: " + data.val().losses);
+
+}
+
+//loads names and scores on refresh
+gameRef.once('value', function(snapshot) {
+    if (snapshot.child('player1').exists()) {
+        var player1 = snapshot.child('player1');
+        fillBox(player1,"player1");
+    }
+    if (snapshot.child('player2').exists()) {
+        var player2 = snapshot.child('player2');
+        fillBox(player2,"player2");
+    }
+});
 
 //database FUNCTIONS
 //Game
 gameRef.on('value', function (snapshot){
-    var player1Name = "";
-    var player2Name = "";
-    var turn = parseInt(snapshot.val().turn);
-
     //there are no players in the game
     if (!snapshot.child('player1').exists()) {
-
         $("#player1").html("Waiting for Player 1");
     }
+    else if ($("#player1").html() === "Waiting for Player 1") {
+        var player1 = snapshot.child('player1');
+        fillBox(player1,"player1");
+    }
 
+    //player 1 is in the game but player 2 is not
     if (!snapshot.child('player2').exists()) {
         $("#player2").html("Waiting for Player 2");
     }
-
-    if (snapshot.child('player1').exists()) {
-        var rock1 = $("<button>").text("Rock").attr({
-            'data-attribute':'rock',
-            'class':'btn btn-light'
-        });
-
-        var paper1 = $("<button>").text("Paper").attr({
-            'data-attribute':'paper',
-            'class':'btn btn-light'
-        });
-        var scissors1 = $("<button>").text('Scissors').attr({
-            'data-attribute':'paper',
-            'class':'btn btn-light'
-        });
-
-
-        player1Name = snapshot.child('player1').val().name;
-        $("#player1").html("<h2>"+player1Name+"</h2>" +
-
-    "Wins: " + snapshot.child('player1').val().wins+" | Losses: " + snapshot.child('player1').val().losses);
-
-        $("#player1").after(rock1);
-        rock1.after(paper1);
-        paper1.after(scissors1);
+    else if ($("#player2").html() === "Waiting for Player 2") {
+        var player2 = snapshot.child('player2');
+        fillBox(player2,"player2");
     }
 
+    $("#player1 #player1Score").html("Wins: " + snapshot.child('player1').val().wins +
+    " | Losses: " + snapshot.child('player1').val().losses);
 
-    if (snapshot.child('player2').exists()) {
-        var rock2 = $("<button>").text("Rock").attr({
-            'data-attribute':'rock',
-            'class':'btn btn-light'
-        });
+    $("#player2 #player2Score").html("Wins: " + snapshot.child('player2').val().wins +
+    " | Losses: " + snapshot.child('player2').val().losses);
 
-        var paper2 = $("<button>").text("Paper").attr({
-            'data-attribute':'paper',
-            'class':'btn btn-light'
-        });
-
-        var scissors2 = $("<button>").text('Scissors').attr({
-            'data-attribute':'paper',
-            'class':'btn btn-light'
-        });
-
-        player2Name = snapshot.child('player2').val().name;
-        $("#player2").html("<h2>"+player2Name+"</h2>" +
-
-        "Wins: " + snapshot.child('player2').val().wins+" | Losses: " + snapshot.child('player2').val().losses);
-
-        $("#player2").after(rock2);
-        rock2.after(paper2);
-        paper2.after(scissors2);
-    }
-
+//===============================================
     //if turn = 0; this is player1's turn to begin
-    if (turn === 0) {
+    // if (turn === 0) {
+    //
+    //     // Player 1's turn
+    //     if ((turn%2 === 0) || (turn === 0)) {
+    //         //$().html("Hi" player1Name "You are player 1. it's your turn")
+    //     }
+    //     // Player 2's turn
+    //     else if (turn%2 != 0) {
+    //         //$().html("Hi" player2Name "You are player 2. It's your turn")
+    //     }
+    // }
+//===============================================
+});
 
-        // Player 1's turn
-        if ((turn%2 === 0) || (turn === 0)) {
-            //$().html("Hi" player1Name "You are player 1. it's your turn")
+
+function gameResult(player1,player2) {
+    gameRef.once('value',function(snapshot){
+
+        var player1 = snapshot.val().player1;
+        var player2 = snapshot.val().player2;
+
+        var winner = 'draw';
+
+        if (player1.choice == 'rock') {
+            if (player2.choice == 'paper') {
+                winner = player2.name + ' wins!';
+                gameRef.child('player2').update({wins:player2.wins+1});
+                gameRef.child('player1').update({losses:player1.losses+1});
+            }
+            if (player2.choice == 'scissors') {
+                winner = player1.name + ' wins!';
+                gameRef.child('player1').update({wins:player1.wins+1});
+                gameRef.child('player2').update({losses:player2.losses+1});
+            }
         }
-        // Player 2's turn
-        else if (turn%2 != 0) {
-            //$().html("Hi" player2Name "You are player 2. It's your turn")
+        else if (player1.choice == 'paper') {
+            if (player2.choice == 'rock') {
+                winner = player1.name + ' wins!';
+                gameRef.child('player1').update({wins:player1.wins+1});
+                gameRef.child('player2').update({losses:player2.losses+1});
+            }
+            if (player2.choice == 'scissors') {
+                winner = player2.name + ' wins!';
+                gameRef.child('player2').update({wins:player2.wins+1});
+                gameRef.child('player1').update({losses:player1.losses+1});
+            }
         }
-    }
+        else if (player1.choice == 'scissors') {
+            if (player2.choice == 'rock') {
+                winner = player2.name + ' wins!';
+                gameRef.child('player2').update({wins:player2.wins+1});
+                gameRef.child('player1').update({losses:player1.losses+1});
+            }
+            if (player2.choice == 'paper') {
+                winner = player1.name + ' wins!';
+                gameRef.child('player1').update({wins:player1.wins+1});
+                gameRef.child('player2').update({losses:player2.losses+1});
+            }
+
+        }
+
+        $("#roundResult").html(winner);
+    });
+
+    $("#player1 .btn").toggle();
+    $("#player2 .btn").toggle();
+    $(".playerSelection").remove();
+
+}
+
+$("#player1").on('click',".btn", function() {
+    var selection = $(this).attr('data-attribute');
+    $("#player1 .btn").toggle();
+    $("#player1Selections").append("<h2 class='playerSelection'>"+selection+"</h2>");
+
+    var player1 = gameRef.child('player1');
+    player1.update({choice:selection});
 
 
 });
 
-        //players
+$("#player2").on('click',".btn", function() {
+    var selection = $(this).attr('data-attribute');
+    $("#player2 .btn").toggle();
+    $("#player2Selections").append("<h2 class='playerSelection'>"+selection+"</h2>");
+    var player2 = gameRef.child('player2');
+    player2.update({choice:selection});
 
-            //name
-            //wins
-            //losses
-            // choice
-
-        //turn
-
-
-    //chat
+    gameResult();
+});
 
 
+//chat
+var chatButton = function(user,message) {
+    // moment.unix();
+    // time.format('LT');
+    // console.log(time);
+
+    gameRef.child('chat').push({
+        name:user,
+        message:message,
+        time:moment()
+    });
+};
+$("#chatSend").on('click',function() {
+    var message = $("#chatMessage").val();
+    chatButton("user",message);
+    $("#chatMessage").val("");
+});
+$("#chatMessage").keypress(function(e){
+    if (e.which == 13) {
+        var message = $("#chatMessage").val();
+        chatButton("user",message);
+        $("#chatMessage").val("");
+
+    }
+});
+
+chatRef.on('child_added',function(snapshot){
+    var chats = snapshot.val();
+    console.log(chats.time);
+    $("#chat tbody").append("<tr><td>" + chats.name+"</td><td>" + chats.message+"</td><td>"+chats.time+"</td></tr>");
+});
+
+chatRef.on('child_removed', function(snapshot) {
+    $("#chat tbody").empty();
+});
 
 
 
 
 
 
-
+var day = moment.unix(1534984460101);
+day.format('LT');
+// console.log(day.format('LT'));
 
 
 //===================================== PROCESS
